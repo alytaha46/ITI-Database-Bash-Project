be627cee42b90667a28c1458e4ebaf1e0697395c
minasaiedbasta <mina.saied.basta@gmail.com>
2023-04-08 19:18:29 +0200

    numbering the database list

--- scripts/list_databases.sh
+++ scripts/list_databases.sh
@@ -4,5 +4,10 @@
 if [ "$(ls -1 ./databases/ | wc -l)" -eq 0 ]; then
   echo "No Databases found"
 else
-  ls -1 ./databases/
+  counter=1
+  for database_name in $(ls -1 ./databases/)
+  do
+    echo "$counter) $database_name"
+    counter=$((counter+1))
+  done
 fi
